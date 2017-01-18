e3d4be90be8ebbf1f1b7d4f4a71d3aca5ee6a4c1
v-mxiaox <v-mxiaox@CNV-MXIAOX-W10>
2017-01-18 16:01:47 +0800

    add code

---
+++ XamarinAndroidDeepLink/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace XamarinAndroidDeepLink
+{
+    [Activity(Label = "XamarinAndroidDeepLink", MainLauncher = true, Icon = "@drawable/icon")]
+    [IntentFilter(new[] { Android.Content.Intent.ActionView },
+    DataScheme = "wori",
+    DataHost = "example.com",
+    DataPathPrefix ="/",
+    Categories = new[] { Android.Content.Intent.CategoryDefault,Android.Content.Intent.CategoryBrowsable })]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+           
+
+        }
+
+       
+    }
+}
+
